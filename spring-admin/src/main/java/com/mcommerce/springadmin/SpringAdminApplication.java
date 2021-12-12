5752125c7950903e0ffcaaaa94d6392355ac829e
yibizzi <ibizzi.younes@ine.inpt.ma>
2021-12-12 18:59:36 +0100

    added spring admin configurations

---
+++ spring-admin/src/main/java/com/mcommerce/springadmin/SpringAdminApplication.java
@@ -0,0 +1,17 @@
+package com.mcommerce.springadmin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
+public class SpringAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdminApplication.class, args);
+    }
+
+}
